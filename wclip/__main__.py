d6c2f2b42af872fc51d78bd40e5a5545e9b2e5ec
MZ AnjumHaQ Heemel <zuhayerror3i8@gmail.com>
2026-04-09 07:50:16 +0600

    refactor: split weather logic into separate fetch and display functions

--- wclip/__main__.py
+++ wclip/__main__.py
@@ -1,4 +1,4 @@
-from .main import weather
+from .main import main
 
 if __name__ == "__main__":
-    weather()
+    main()
--- wclip/main.py
+++ wclip/main.py
@@ -5,31 +5,46 @@ import click
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city, api_key):
+    """
+    Fetches current weather data.
+    """
+    request_url = f"{BASE_URL}?q={city}&appid={api_key}&units=metric"
+
+    response = requests.get(request_url)
+
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def display_weather(data):
+    """
+    Displays current weather data.
+    """
+    if data:
+        click.echo(json.dumps(data, indent=2))
+    else:
+        click.echo("An unexpected error occurred!")
 
 @click.command()
 @click.argument("city", type=str)
-def weather(city):
+def main(city):
     """
     Fetches and displays current weather data for a specified CITY.
     """
+    load_dotenv()
+
     api_key = os.getenv("OWM_API_KEY")
 
     if not api_key:
         click.echo("Error: OWM_API_KEY environment variable is missing.")
         sys.exit(1)
 
-    request_url = f"{BASE_URL}?q={city}&appid={api_key}&units=metric"
+    data = get_weather(city, api_key)
 
-    response = requests.get(request_url)
-
-    if response.status_code == 200:
-        data = response.json()
-        click.echo(json.dumps(data, indent=2))
-    else:
-        click.echo("An unexpected error occurred!")
+    display_weather(data)
 
 if __name__ == "__main__":
-    weather()
+    main()
